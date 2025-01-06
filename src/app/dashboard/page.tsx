3001d00763037ca0e7ec47c9401f21c84ba89381
PhamHoang16 <hoangpham1618@gmail.com>
2025-01-07 01:36:50 +0700

    basic filter

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,19 +1,20 @@
-"use client";
+'use client';
 
-import { useRouter } from "next/navigation"; // Thay đổi import
-import React, { useEffect, useState } from "react";
-import { Container, CircularProgress } from "@mui/material";
-import { CategorySection } from "@/components/dashboard/categories/categories-card";
-import { Category } from "@/types/category";
-import Button from "@mui/material/Button";
-import { Plus as PlusIcon } from "@phosphor-icons/react/dist/ssr/Plus";
-import Stack from "@mui/material/Stack";
-import Grid from "@mui/material/Grid";
-import Box from "@mui/material/Box";
-import {MainList} from "@/components/dashboard/main-list";
-import {Order} from "@/types/order";
-import {Deposit} from "@/types/deposit";
-import axios from "axios";
+import React, { useEffect, useState } from 'react';
+import { useRouter } from 'next/navigation';
+import { ListMainItem } from '@/app/dashboard/page';
+import { CircularProgress, Container, FormControl, InputLabel, MenuItem, Select } from '@mui/material';
+import Box from '@mui/material/Box';
+import Button from '@mui/material/Button';
+import Card from '@mui/material/Card';
+import Grid from '@mui/material/Grid';
+import Stack from '@mui/material/Stack';
+import { Plus as PlusIcon } from '@phosphor-icons/react/dist/ssr/Plus';
+import axios from 'axios';
+
+import { Category } from '@/types/category';
+import { CategorySection } from '@/components/dashboard/categories/categories-card';
+import { MainList } from '@/components/dashboard/main-list';
 
 export interface ListMainItem {
   username: string;
@@ -23,28 +24,28 @@ export interface ListMainItem {
 
 export default function Page(): React.JSX.Element {
   const [categories, setCategories] = useState<Category[]>([]);
+  const [selectedCategory, setSelectedCategory] = useState<string>('');
   const [lastOrder, setLastOrder] = useState<ListMainItem[]>([]);
   const [lastDeposit, setLastDeposit] = useState<ListMainItem[]>([]);
   const [loading, setLoading] = useState<boolean>(true);
-  const router = useRouter(); // Sử dụng useRouter từ next/navigation
+  const router = useRouter();
 
   useEffect(() => {
     const fetchCategories = async () => {
       try {
-        const response = await fetch("http://localhost:8080/products");
+        const response = await fetch('http://localhost:8080/products');
         const data: any[] = await response.json();
 
-        // Map dữ liệu vào đúng interface Category
         const formattedCategories: Category[] = data.map((category) => ({
           name: category.name,
           products: category.productList.map((product: any) => ({
             id: product.id,
             categoryId: product.categoryId,
             name: product.name,
-            description: product.description.split("\n"), // Tách mô tả thành mảng
-            price: new Intl.NumberFormat("vi-VN", {
-              style: "currency",
-              currency: "VND",
+            description: product.description.split('\n'),
+            price: new Intl.NumberFormat('vi-VN', {
+              style: 'currency',
+              currency: 'VND',
             }).format(product.price),
             quantity: product.quantity,
           })),
@@ -52,7 +53,7 @@ export default function Page(): React.JSX.Element {
 
         setCategories(formattedCategories);
       } catch (error) {
-        console.error("Error fetching categories:", error);
+        console.error('Error fetching categories:', error);
       } finally {
         setLoading(false);
       }
@@ -60,38 +61,46 @@ export default function Page(): React.JSX.Element {
 
     const fetchOrders = async () => {
       try {
-        const response = await axios.get("http://localhost:8080/products/orders/last-orders");
-        setLastOrder(response.data); // Assuming the response data matches the expected order format
+        const response = await axios.get('http://localhost:8080/products/orders/last-orders');
+        setLastOrder(response.data);
       } catch (error) {
-        console.error("Error fetching orders:", error);
+        console.error('Error fetching orders:', error);
       }
     };
     const fetchDeposit = async () => {
       try {
-        const response = await axios.get("http://localhost:8080/users/deposit/last-deposit");
+        const response = await axios.get('http://localhost:8080/users/deposit/last-deposit');
         setLastDeposit(response.data);
       } catch (error) {
-        console.error("Error fetching orders:", error);
+        console.error('Error fetching orders:', error);
       }
     };
 
     fetchCategories();
     fetchOrders();
-    fetchDeposit()
+    fetchDeposit();
   }, []);
 
   const handleAddClick = () => {
-    router.push("/product/add"); // Điều hướng
+    router.push('/product/add');
+  };
+
+  const handleCategoryChange = (event: React.ChangeEvent<{ value: unknown }>) => {
+    setSelectedCategory(event.target.value as string);
   };
 
   if (loading) {
     return <CircularProgress />;
   }
 
-  // @ts-ignore
+  const filteredCategories = selectedCategory
+    ? categories.filter((category) => category.name === selectedCategory)
+    : categories;
+
   return (
     <Stack spacing={2}>
-        <div>
+      <Card>
+        <Box sx={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center', padding: 2 }}>
           <Button
             startIcon={<PlusIcon fontSize="var(--icon-fontSize-md)" />}
             variant="contained"
@@ -99,28 +108,41 @@ export default function Page(): React.JSX.Element {
           >
             Add
           </Button>
-        </div>
-        {categories.map((category, index) => (
-          <CategorySection
-            key={index}
-            name={category.name}
-            products={category.products}
-          />
-        ))}
-        <Grid container spacing={1}>
-          <Grid item xs={12} md={6} >
-              <Box>
-                <MainList title={"Lịch sử mua hàng"} rows={lastOrder}>
-                </MainList>
-              </Box>
-          </Grid>
-          <Grid item xs={12} md={6}>
-              <Box>
-                <MainList title={"Lịch sử nạp tiền"} rows={lastDeposit}>
-                </MainList>
-              </Box>
-          </Grid>
+          <FormControl sx={{ minWidth: 350 }}>
+            <InputLabel id="category-select-label">Danh mục</InputLabel>
+            <Select
+              labelId="category-select-label"
+              value={selectedCategory}
+              onChange={handleCategoryChange}
+              label="Category"
+            >
+              <MenuItem value="">
+                <em>Tất cả</em>
+              </MenuItem>
+              {categories.map((category) => (
+                <MenuItem key={category.name} value={category.name}>
+                  {category.name}
+                </MenuItem>
+              ))}
+            </Select>
+          </FormControl>
+        </Box>
+      </Card>
+      {filteredCategories.map((category, index) => (
+        <CategorySection key={index} name={category.name} products={category.products} />
+      ))}
+      <Grid container spacing={1}>
+        <Grid item xs={12} md={6}>
+          <Box>
+            <MainList title={'Lịch sử mua hàng'} rows={lastOrder} />
+          </Box>
         </Grid>
+        <Grid item xs={12} md={6}>
+          <Box>
+            <MainList title={'Lịch sử nạp tiền'} rows={lastDeposit} />
+          </Box>
+        </Grid>
+      </Grid>
     </Stack>
   );
 }
